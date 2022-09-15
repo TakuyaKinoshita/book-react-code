dd546f96196697c5e5dd730c6bd0106171036f84
TakuyaKinsohita <kinoshita@lm-labs.com>
2022-09-15 15:05:11 +0900

    add css styles

--- my-react-apps/my-wiki/src/App.jsx
+++ my-react-apps/my-wiki/src/App.jsx
@@ -1,13 +1,26 @@
 export const App = () => {
+  const styles = {
+    title: {
+      color: "red",
+      fontSize: "2rem",
+      textAlign: "center",
+      display: "block"
+    },
+    text: {
+      color: "#323232",
+      fontSize: "1.1rem"
+    }
+  };
+
   const onClickButton = () => {
     alert("Click Button!");
-  }
+  };
 
   return (
     <>
-      <h1>Welcome to My wiki what`s worked on React!</h1>
-      <p>you can to render dom Elements by empty tags</p>
+      <h1 style={styles.title}>Welcome to My wiki what`s worked on React!</h1>
+      <p style={styles.text}>you can to render dom Elements by empty tags</p>
       <button onClick={onClickButton}>サンプルボタン</button>
     </>
-  )
+  );
 }
