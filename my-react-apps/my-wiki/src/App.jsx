6786d11a3adfbb92b2fb221596e57cf05065d77a
TakuyaKinsohita <kinoshita@lm-labs.com>
2022-09-15 14:55:40 +0900

    add Button what`s pop up alert on click button

--- my-react-apps/my-wiki/src/App.jsx
+++ my-react-apps/my-wiki/src/App.jsx
@@ -1,8 +1,13 @@
 export const App = () => {
+  const onClickButton = () => {
+    alert("Click Button!");
+  }
+
   return (
     <>
       <h1>Welcome to My wiki what`s worked on React!</h1>
       <p>you can to render dom Elements by empty tags</p>
+      <button onClick={onClickButton}>サンプルボタン</button>
     </>
   )
 }
