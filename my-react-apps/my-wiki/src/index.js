56992a7c79f6e79708924a4a6738f2e35b66fe9d
TakuyaKinsohita <kinoshita@lm-labs.com>
2022-09-15 14:47:09 +0900

    test empty tag

--- my-react-apps/my-wiki/src/index.js
+++ my-react-apps/my-wiki/src/index.js
@@ -1,7 +1,12 @@
 import ReactDom from "react-dom";
 
 const App = () => {
-  return null;
+  return (
+    <>
+      <h1>Welcome to My wiki what`s worked on React!</h1>
+      <p>you can to render dom Elements by empty tags</p>
+    </>
+  )
 }
 
 ReactDom.render(<App />, document.getElementById("root"));
