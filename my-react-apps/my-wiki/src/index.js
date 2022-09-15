b9db495b5e4dcb75490daf43046aee747741fbb2
TakuyaKinsohita <kinoshita@lm-labs.com>
2022-09-15 14:38:12 +0900

    render h1 dom elements

--- my-react-apps/my-wiki/src/index.js
+++ my-react-apps/my-wiki/src/index.js
@@ -1,7 +1,8 @@
 import ReactDom from "react-dom";
 
 const App = () => {
-  return null;
+  // return null;
+  return <h1>Hellp My Wiki what`s working on React!</h1>
 }
 
 ReactDom.render(<App />, document.getElementById("root"));
