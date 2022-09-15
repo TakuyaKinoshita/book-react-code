094e093a876f2b94049f0bf77ad276a4146677b5
TakuyaKinsohita <kinoshita@lm-labs.com>
2022-09-15 14:41:29 +0900

    use fragment

--- my-react-apps/my-wiki/src/index.js
+++ my-react-apps/my-wiki/src/index.js
@@ -1,8 +1,14 @@
 import ReactDom from "react-dom";
+import { Fragment } from "react";
 
 const App = () => {
   // return null;
-  return <h1>Hellp My Wiki what`s working on React!</h1>
+  return (
+    <Fragment>
+      <h1>Hellp My Wiki what`s working on React!</h1>
+      <p>you can be enclosed dom elements by Fragment</p>
+    </Fragment>
+  );
 }
 
 ReactDom.render(<App />, document.getElementById("root"));
