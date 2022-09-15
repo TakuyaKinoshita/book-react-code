b3048491b16d9f678e63734234872148d6107b5d
TakuyaKinsohita <kinoshita@lm-labs.com>
2022-09-15 14:45:20 +0900

    Revert "render h1 dom elements"
    This reverts commit b9db495b5e4dcb75490daf43046aee747741fbb2.

--- my-react-apps/my-wiki/src/index.js
+++ my-react-apps/my-wiki/src/index.js
@@ -1,14 +1,7 @@
 import ReactDom from "react-dom";
-import { Fragment } from "react";
 
 const App = () => {
-  // return null;
-  return (
-    <Fragment>
-      <h1>Hellp My Wiki what`s working on React!</h1>
-      <p>you can be enclosed dom elements by Fragment</p>
-    </Fragment>
-  );
+  return null;
 }
 
 ReactDom.render(<App />, document.getElementById("root"));
